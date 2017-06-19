a6295b7522fdfa9c7f3cd5e7ca72bca9437c6d77
Dmitry <DetSimen@users.noreply.github.com>
2017-06-19 11:58:59 +0700

    Update DEF_Message.h

--- DEF_Message.h
+++ DEF_Message.h
@@ -2,28 +2,28 @@
 
 // сообщения, выделенные под системные
 
-#define  msg_Empty 0x00
-#define  msg_Error 0x01
-#define  msg_Paint 0x02
+#define  msg_Empty      0x00  // пустое сообщение
+#define  msg_Error      0x01  // сообщение, сигнализирующее об ошибке
+#define  msg_Paint      0x02  // сообщение перерисовать экран. 
 
 // сообщения пользователя, даны только для примера, создавайте свои с номерами 0x10-0xFF
 // удобно группировать сообщения по группам
 
 // группа разное. 
 
-#define  msg_SecondTick			  0x10    // пришел секундный тик
-#define	 msg_ReadMQ2			    0x12	  // читать данные с датчика MQ2
-#define  msg_MQ2Changed			  0x13	  // показания c MQ2 изменились
+#define  msg_SecondTick     0x10  // пришел секундный тик
+#define	 msg_ReadMQ2        0x12  // читать данные с датчика MQ2
+#define  msg_MQ2Changed     0x13  // показания c MQ2 изменились
 
 // группа клавиатуры
 
-#define  msg_ReadKey			    0x20    // прочитать кнопку с клавы
-#define  msg_KeyDown			    0x21	  // кнопка нажата
-#define	 msg_KeyUp				    0x22	  // кнопка отпущена
+#define  msg_ReadKey        0x20  // прочитать кнопку с клавы
+#define  msg_KeyDown        0x21  // кнопка нажата
+#define	 msg_KeyUp          0x22  // кнопка отпущена
 
 // группа сообщений исполнительных устройств
 
-#define msg_VentON				    0x82	  // включить вытяжку
-#define msg_VentOFF				    0x83	  // выключить вытяжку
+#define msg_VentON          0x82  // включить вытяжку
+#define msg_VentOFF         0x83  // выключить вытяжку
 
 
