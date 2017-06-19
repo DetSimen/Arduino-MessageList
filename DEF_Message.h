4fe1e9fae77323497a5c4cb24a2091680610af9c
Dmitry <DetSimen@users.noreply.github.com>
2017-06-19 11:55:31 +0700

    Update DEF_Message.h

--- DEF_Message.h
+++ DEF_Message.h
@@ -1,23 +1,29 @@
 ﻿#pragma once
 
-// system messages
+// сообщения, выделенные под системные
 
 #define  msg_Empty 0x00
 #define  msg_Error 0x01
 #define  msg_Paint 0x02
 
-// user messages
+// сообщения пользователя, даны только для примера, создавайте свои с номерами 0x10-0xFF
+// удобно группировать сообщения по группам
 
-#define  msg_SecondTick			0x10
-#define  msg_PrintRelayState	0x11
-#define	 msg_ReadMQ2			0x12	//читать данные с датчика MQ2
-#define  msg_MQ2Changed			0x13	// показания c MQ2 изменились
-#define  msg_ReadKey			0x14
+// группа разное. 
 
-#define  msg_KeyDown			0x20	// кнопка нажата
-#define	 msg_KeyUp				0x21	// кнопка отпущена
+#define  msg_SecondTick			  0x10    // пришел секундный тик
+#define	 msg_ReadMQ2			    0x12	  // читать данные с датчика MQ2
+#define  msg_MQ2Changed			  0x13	  // показания c MQ2 изменились
 
-#define msg_VentON				0x22	// включить вытяжку
-#define msg_VentOFF				0x23	// выключить вытяжку
+// группа клавиатуры
+
+#define  msg_ReadKey			    0x20    // прочитать кнопку с клавы
+#define  msg_KeyDown			    0x21	  // кнопка нажата
+#define	 msg_KeyUp				    0x22	  // кнопка отпущена
+
+// группа сообщений исполнительных устройств
+
+#define msg_VentON				    0x82	  // включить вытяжку
+#define msg_VentOFF				    0x83	  // выключить вытяжку
 
 
